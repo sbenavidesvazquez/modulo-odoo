3f1be7fb98f65358d1a60756c713b37621ca3aff
samsung <sbenavidesvazquez@danielcastelao.org>
2016-03-09 09:53:25 +0100

    modificar models.py

--- openacademy/models/models.py
+++ openacademy/models/models.py
@@ -2,13 +2,13 @@
 
 from openerp import models, fields, api
 
-# class openacademy(models.Model):
-#     _name = 'openacademy.openacademy'
-
-#     name = fields.Char()
+class course(models.Model):
+    _name = 'openacademy.course'
+    
+    name = fields.Char(string="Title", required=True)
 #     value = fields.Integer()
 #     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
+    description = fields.Text()
 #
 #     @api.depends('value')
 #     def _value_pc(self):
