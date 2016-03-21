aa70c591bed2396fe9dbba4b31c7c65e71b7b62a
samsung <sbenavidesvazquez@danielcastelao.org>
2016-03-21 12:04:26 +0100

    Añadir vistas de sesion

--- openacademy/models.py
+++ openacademy/models.py
@@ -8,6 +8,8 @@ class Course(models.Model):
     name = fields.Char(string="Titulo", required=True)
     description = fields.Text()
     
+    responsible_id = fields.Many2one('res.users',
+        ondelete='set null', string="Responsible", index=True)
     
 class Session(models.Model):
     _name = 'openacademy.session'
@@ -16,4 +18,8 @@ class Session(models.Model):
     start_date = fields.Date()
     duration = fields.Float(digits=(6, 2), help="Duration in days")
     seats = fields.Integer(string="Number of seats")
+    
+    instructor_id = fields.Many2one('res.partner', string="Instructor")
+    course_id = fields.Many2one('openacademy.course',
+        ondelete='cascade', string="Course", required=True)
   
